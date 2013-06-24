71ea0fbf96598c95374922a2f4b0b5f018c77a17
Sridhar Sarnobat <ss401533@gmail.com>
2013-06-23 21:41:59 -0700

    first commit

--- server/webserver.groovy
+++ server/webserver.groovy
@@ -1,3 +1,11 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -30,6 +38,24 @@ class MyHandler implements HttpHandler {
 	}  
 
 	public void handle(HttpExchange t) throws IOException {
+		final String SERVER_ROOT_URI = "http://localhost:7474/db/data/";
+		final String nodeEntryPointUri = SERVER_ROOT_URI + "node";
+		// http://localhost:7474/db/data/node
+		 
+		WebResource resource = Client.create().resource( nodeEntryPointUri );
+		
+		// POST {} to the node entry point URI
+		ClientResponse response = resource.accept( MediaType.APPLICATION_JSON )
+				.type( MediaType.APPLICATION_JSON )
+				.entity( "{ }" )
+				.post( ClientResponse.class );
+		 
+		final URI location = response.getLocation();
+		System.out.println( String.format(
+				"POST to [%s], status code [%d], location header [%s]",
+				nodeEntryPointUri, response.getStatus(), location.toString() ) );
+		response.close();
+	
 		JSONObject json = new JSONObject();
 		String query = t.getRequestURI();
 		Map<String, String> map = getQueryMap(query);  
