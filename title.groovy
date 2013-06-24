930578885029b96710683aa6fe3468d6eb2fd540
Sridhar Sarnobat <ss401533@gmail.com>
2013-06-23 21:41:59 -0700

    first commit

--- title.groovy
+++ title.groovy
@@ -1,16 +1,20 @@
 import org.htmlcleaner.*;
 
 
-HttpURLConnection con = (HttpURLConnection) new URL("http://www.yahoo.com").openConnection();
-con.connect();
+String getTitle(String url) {
+	HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+	con.connect();
+	
+	InputStream inputStream = con.getInputStream();
+	
+	HtmlCleaner cleaner = new HtmlCleaner();
+	CleanerProperties props = cleaner.getProperties();
+	
+	TagNode node = cleaner.clean(inputStream)
+	TagNode titleNode = node.findElementByName("title", true);
+	
+	String title = titleNode.getText().toString();
 
-InputStream inputStream = con.getInputStream();
-
-HtmlCleaner cleaner = new HtmlCleaner();
-CleanerProperties props = cleaner.getProperties();
-
-TagNode node = cleaner.clean(inputStream)
-TagNode titleNode = node.findElementByName("title", true);
-
-String title = titleNode.getText().toString();
-println(title)
+	return title;
+}
+println(getTitle("http://www.yahoo.com"));
