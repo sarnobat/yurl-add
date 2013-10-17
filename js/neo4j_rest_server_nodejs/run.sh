ca50b167e75b3c04ad77d5abec97d8e86a0b8884
sarnobat <ss401533@gmail.com>
2013-10-16 18:52:36 -0700

    Removed node js

--- js/neo4j_rest_server_nodejs/run.sh
+++
@@ -1,3 +0,0 @@
-
-cd /home/sarnobat/Desktop/new/github/yurl_rest/js/neo4j_rest_server_nodejs
-node  server.js
--- js/neo4j_rest_server_nodejs/test.js
+++
@@ -1,8 +0,0 @@
- var page = require('webpage').create();
-        page.open('http://www.teamtalk.com', function (status) {
-                  var title = page.evaluate(function () {
-                            return document.title;
-                        });
-                      console.log('Page title is ' + title);
-                  });
-
