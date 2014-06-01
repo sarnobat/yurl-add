bf14bc9d5923de101ae38a863d229d797107c4a2
Sridhar Sarnobat <ss401533@gmail.com>
2014-05-31 21:55:35 -0700

    Minor

--- imageLinkFinder.js
+++ imageLinkFinder.js
@@ -1,28 +1,115 @@
-var $ = require('cheerio')
 var request = require('request')
+var $ = require('cheerio')
+var url=require('url');
+var sleep = require('sleep');
+expect = require('expect.js')
+var neo4j = require('neo4j')
 
-function gotHTML(err, resp, html) {
+	var biggestImages = {};
+var targetUrl = 'http://www.teamtalk.com/liverpool'
 
-  if (err) return console.error(err)
-	console.log('goHTML');
-  // get all img tags and loop over them
-  var imageURLs = []
-  	var parsedHTML = $.load(html)
 
-  var b = parsedHTML('img').map(function(i, link) {
-    var href = $(link).attr('src');
-	imageURLs.push(domain + href);
-	imageURLs.push(href);
-	//console.log(imageURLs[0]);
-  });
-  
-  //console.log(imageURLs);
-  for (var i = 0; i < imageURLs.length; i++) {
- 	console.log("<img src='" + imageURLs[i] + "'/>");
-  }
-  
-  // TODO: Sort the images by size from the http content-length header
+var express = require('express');
+
+var app = express();
+
+getBiggestImage("http://lifehacker.com/5330687/items-you-can-get-great-deals-on-in-a-recession");
+
+app.get('/setUrls', function(req, res) {
+	//getBiggestImage(targetUrl)
+    res.send({"status" : "success" });
+});
+
+app.get('/biggestImages', function(req, res) {
+    res.send(biggestImages);
+});
+
+
+function getBiggestImage(urlToFindBiggestImage) {
+
+
+	var domain =  url.parse(urlToFindBiggestImage).hostname;
+	
+	var allImages = {}; // We're not using this
+	var allImagesArray = [];
+		
+	function gotHTML(err, resp, html) {
+		allImages = {};
+		allImagesArray = [];
+		if (err) {
+			return console.error(err) ;
+		}
+		
+		var imageURLs = []
+		var parsedHTML = $.load(html)
+	
+		var b = parsedHTML('img').map(function(i, link) {
+		var href = $(link).attr('src');
+		if (href == null) {
+			// TODO: not sure why this doesn't find images, I can see them in the HTML
+			console.log("Can't find image: " + urlToFindBiggestImage);
+			return;
+		}
+		if (href.indexOf('/') === 0) {
+			imageURLs.push(domain + href);
+		} else {
+			imageURLs.push(href);
+		}
+	});
+	
+	for (var i = 0; i < imageURLs.length; i++) {
+		var imgUrl = imageURLs[i];
+		
+		(function(theUrl, idx) {
+			request(theUrl, function (err, res, body){
+				if (res == null) {
+					console.log("null response from: " + theUrl);
+					return;
+				}
+				var entry =  {
+				 "length" : parseInt(res.headers['content-length']),
+				 "url" : theUrl,
+				};	
+				allImages[theUrl] = entry;
+				allImagesArray.push(entry);
+		
+			}); 		
+		})(imgUrl,i);
+	  }
+	}
+	
+	function compare(a,b) {
+	  if (a.length < b.length)
+		 return 1;
+	  if (a.length > b.length)
+		return -1;
+	  return 0;
+	}
+	
+	request(urlToFindBiggestImage, gotHTML)
+	
+	setTimeout(function() {
+		if (allImagesArray < 1) {
+			return;
+		}
+		var allImagesArraySorted = allImagesArray.sort(compare);
+		console.log(JSON.stringify(allImagesArraySorted));
+		biggestImages[urlToFindBiggestImage] = allImagesArraySorted[0].url;
+	}, 5000);
+		
 }
 
-var domain = 'http://www.teamtalk.com/'
-request(domain, gotHTML)
+var db = new neo4j.GraphDatabase('http://netgear.rohidekar.com:7474');
+
+results = db.query ('start n=node(45) match n-->c where has(c.title) return c.url as url,c.title as title limit 50', function(err, result) {
+        if(err) throw err;
+        for (var j = 0; j < result.length; j++) {
+        	if (result[j] == null) {
+        		continue;
+        	}
+			getBiggestImage(result[j].url); // delivers an array of query results
+		}
+});
+
+app.listen(3000);
+console.log('Listening on port http://localhost:3000');
