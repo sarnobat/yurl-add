d0dc446418b2adf7d29c1461a5c3c219d4ba4323
Sridhar Sarnobat <ss401533@gmail.com>
2016-11-29 20:44:40 -0800

    Minor changes (untested)

--- headless_chrome_selenium/run.sh
+++ headless_chrome_selenium/run.sh
@@ -1,1 +1,3 @@
-groovy headless.groovy 'www.google.com' | tee out.html
+groovy headless.groovy 'www.bbc.co.uk' | tee out.html
+groovy biggest_image.groovy 'www.teamtalk.com' | tee biggest_image.txt
+
