8c66c7d6a0159511b94c25931f21b271816d6af3
ssarnobat <ss401533@gmail.com>
2016-12-10 14:04:05 -0800

    Minor changes (untested)

--- list_neo4j.sh
+++ list_neo4j.sh
@@ -1,5 +1,5 @@
 curl -X POST -H 'Content-type: application/json' \
   'http://netgear.rohidekar.com:7474/db/data/cypher' -d '
   {
-     "query": "START n=node('$1') MATCH n-->c WHERE HAS(c.url) RETURN id(n) + '\''::'\'' + c.created + '\''::'\'' + c.url order by c.created asc", "params":{}
+     "query": "START n=node(*) MATCH n-->c WHERE HAS(c.url) and HAS(n.type) RETURN id(n) + '\''::'\'' + c.created + '\''::'\'' + c.url order by c.created asc", "params":{}
   }'
