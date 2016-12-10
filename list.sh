c9da3d9cd5d89fe097ca02b4cf155e35764a6c05
ssarnobat <ss401533@gmail.com>
2016-12-10 13:48:23 -0800

    Minor changes (untested)

--- list.sh
+++ list.sh
@@ -1,5 +1,5 @@
 curl -X POST -H 'Content-type: application/json' \
   'http://netgear.rohidekar.com:7474/db/data/cypher' -d '
   {
-     "query": "START n=node(37658) MATCH n-->c WHERE HAS(c.url) AND NOT HAS(c.downloaded_video) RETURN c.created,c.url order by c.created asc", "params":{}
+     "query": "START n=node(37658) MATCH n-->c WHERE HAS(c.url) RETURN c.created,c.url order by c.created asc", "params":{}
   }'
