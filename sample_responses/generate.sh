1979d1138bae4a4e9f9778fdc05e043a23bc4452
Sridhar Sarnobat <ss401533@gmail.com>
2017-08-27 17:32:55 -0700

    Minor changes (untested)

--- sample_responses/generate.sh
+++ sample_responses/generate.sh
@@ -3,3 +3,9 @@ groovy yurl_uncategorized.groovy | tee generated.json
 
 cd ~/github/yurl/sample_responses/uncategorized/urls/
 groovy yurl_urls.groovy | tee generated.json
+
+cd ~/github/yurl/sample_responses/uncategorized/categoriesRecursive/
+groovy yurl_categoriesRecursive.groovy | tee generated.json
+
+cd ~/github/yurl/sample_responses/uncategorized/categoriesRecursive/children/
+groovy yurl_children.groovy | tee generated.json
